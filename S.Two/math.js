e69f8acee1390b8ce2b23bc9c9377f682802b486
ANarngAung <anarngaung2-17@gmail.com>
2024-12-30 13:30:07 +0630

    create setup two for test node

---
+++ S.Two/math.js
@@ -0,0 +1,7 @@
+// setup two
+const add = (a, b) => a + b;
+const minus = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+module.exports = {add, minus, multiply, divide,};
---
+++ S.Two/server.js
@@ -0,0 +1,8 @@
+// setup two
+// require is import
+const math = require('./math.js');
+
+console.log(math.add(5, 3));
+console.log(math.minus(5, 3));
+console.log(math.multiply(5, 3));
+console.log(math.divide(5, 3));
